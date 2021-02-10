ea954d35c25618cf7a9eb1e7876adc5c91480ee5
Sam Cowger <sam@galois.com>
2021-02-10 11:45:26 -0800

    Modify server to accept POST requests with JSON payloads

--- AlgebraicPetri-Stratification/server.jl
+++ AlgebraicPetri-Stratification/server.jl
@@ -1,32 +1,27 @@
 using AlgebraicPetri
-# using ArgParse
 using Catlab.Graphs.BasicGraphs
 using Catlab.CategoricalAlgebra
 using JSON
 using Genie
 using Genie.Router
+using Genie.Requests
 using Genie.Renderer.Json
  
 include("ModelStratify.jl")
 
-function load_model(file::String)
-    model_json = open(f->read(f, String), file);
-    model = ModelStratify.deserialize(model_json, LabelledPetriNet);
+function load_model(model_json::Dict)
+    model_str = JSON.json(model_json)
+    model = ModelStratify.deserialize(model_str, LabelledPetriNet);
     model
 end
 
-function load_connection_graph(file::String)
-    conn_json = open(f->read(f, String), file);
-    connections = ModelStratify.deserialize(conn_json, BasicGraphs.Graph);
+function load_connection_graph(conn_json::Dict)
+    conn_str = JSON.json(conn_json)
+    connections = ModelStratify.deserialize(conn_str, BasicGraphs.Graph);
     connections
 end
 
-function load_states(file::String)
-    states_json = open(f->read(f, String), file);
-    states = JSON.parse(states_json);
-
-    println(stderr, states)
-
+function load_states(states::Dict)
     sus = Symbol(states["sus"]);
     exp = Symbol(states["exp"]);
     inf = [Symbol(i) for i = states["inf"]];
@@ -34,7 +29,7 @@ function load_states(file::String)
     sus, exp, inf
 end
 
-function demographic_stratification(topology::String, connections::String, states::String)
+function demographic_stratification(topology::Dict, connections::Dict, states::Dict)
     println(stderr, "performing demographic stratification...")
     model = load_model(topology)
     connections = load_connection_graph(connections)
@@ -44,7 +39,7 @@ function demographic_stratification(topology::String, connections::String, state
     return ModelStratify.serializeToString(demographic_model);
 end
 
-function spatial_stratification(topology::String, connections::String)
+function spatial_stratification(topology::Dict, connections::Dict)
     println(stderr, "performing spatial stratification...")
     model = load_model(topology)
     connections = load_connection_graph(connections)
@@ -53,16 +48,19 @@ function spatial_stratification(topology::String, connections::String)
     return ModelStratify.serializeToString(spatial_model);
 end
 
-route("/") do 
-    if haskey(@params, :type)
-        if @params(:type) == "dem"
-            Json.json(demographic_stratification(@params(:top), @params(:conn), @params(:states)))
-        elseif @params(:type) == "spat"
-            Json.json(spatial_stratification(@params(:top), @params(:conn)))
+route("/", method = POST) do 
+    # @show jsonpayload()
+
+    payload = jsonpayload()
+    if haskey(payload, "type")
+        if payload["type"] == "dem"
+            Json.json(demographic_stratification(payload["top"], payload["conn"], payload["states"]))
+        elseif payload["type"] == "spat"
+            Json.json(spatial_stratification(payload["top"], payload["conn"]))
         end
     end
 end
 
 if abspath(PROGRAM_FILE) == @__FILE__
-    up(8001, async=false)
+    up(8001, "0.0.0.0", async=false)
 end
